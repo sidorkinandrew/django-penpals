54d7c836773644b54fb25a7de6512203aad8d737
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2020-09-28 23:03:29 +0000

    utils.py

---
+++ pages/utils.py
@@ -0,0 +1,9 @@
+LANGUAGE_CHOICES = (
+    ('1', 'English'),
+    ('2', 'Spanish'),
+    ('3', 'German'),
+    ('4', 'Swedish'),
+    ('5', 'Chinese'),
+    ('6', 'Italian'),
+    ('7', 'Polish'),
+)
