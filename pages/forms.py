c00cd404d698211bbeda884c4108295c7c968cff
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2020-10-02 20:22:06 +0000

    values to labels; get_object (ProfileView); get_success_url (ProfileEdit)

--- pages/forms.py
+++ pages/forms.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm 
 from .utils import LANGUAGE_CHOICES
 from .models import Profile
 
@@ -10,6 +10,18 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+
+class ProfileViewForm(UserChangeForm, forms.ModelForm):
+    first = forms.CharField(max_length=100, required=True)
+    last = forms.CharField(max_length=100, required=True)
+    speaks = forms.MultipleChoiceField(choices=LANGUAGE_CHOICES, required=True)
+    learns = forms.MultipleChoiceField(choices=LANGUAGE_CHOICES, required=True)
+
+    class Meta:
+        model = Profile
+        fields = ['first', 'last', 'speaks', 'learns', 'photo']
+
 
 
 class ProfileEditForm(UserChangeForm, forms.ModelForm):
--- pages/views.py
+++ pages/views.py
@@ -7,8 +7,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import ListView, CreateView, DetailView, UpdateView
 from django.urls import reverse_lazy
 from .models import Profile
-from .forms import UserRegisterForm, ProfileEditForm
+from .forms import UserRegisterForm, ProfileEditForm, ProfileViewForm
 from django.contrib import messages
+from .utils import LANGUAGE_CHOICES
 
 def index(request):
     if request.user.is_authenticated:
@@ -31,22 +32,43 @@ def index(request):
 
 class ProfileView(SuccessMessageMixin, DetailView):
     template_name = 'pages/profile.html'
-    form_class = ProfileEditForm
+    form_class = ProfileViewForm
     context_object_name = 'profile'
     model = Profile
-    def get_object(self):
-        return Profile.objects.get(id=self.kwargs['profile_id'])
 
+    def from_value_to_label(self, field):
+        if field is not None:
+            list_of_labels = [label for value, label in LANGUAGE_CHOICES if value in field]
+            return ", ".join(list_of_labels)
+        return field
+    
+    def get_object(self): 
+        instance = Profile.objects.get(id=self.kwargs['profile_id'])  # self.request.user.profile.id)
+        instance.speaks = self.from_value_to_label(instance.speaks)
+        instance.learns = self.from_value_to_label(instance.learns)
+        return instance
 
 class ProfileEdit(SuccessMessageMixin, UpdateView):
     model = Profile
     template_name = 'pages/edit.html'
     form_class = ProfileEditForm
     context_object_name = 'profile'
-    success_message = "Your profile was updated successfully"
+    success_message = "Your profile was updated successfully"  
 
-    def get_object(self):
+    def get_object(self): 
         return Profile.objects.get(id=self.request.user.profile.id) #  self.kwargs['profile_id'])
+
+    def get_success_url(self):
+          profile_id=self.request.user.profile.id
+          return reverse_lazy('pages:profile', kwargs={'profile_id': profile_id})
+
+        #        print(self.request.user.profile.id)
+#        instance.speaks = self.from_value_to_label(instance.speaks)
+#        print(instance.speaks)
+#        print(instance.learns)
+#        print(dir(instance))
+#        return instance
+
 
 def edit_profile(request):
     pass
